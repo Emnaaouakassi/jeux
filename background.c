1b01187e41274c0090f2e942e581160bd4c2338c
jelassiahmed <47889856+jelassiahmed@users.noreply.github.com>
2019-03-24 15:19:46 +0100

    Add files via upload

---
+++ background.c
@@ -0,0 +1,12 @@
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include <string.h>
+#include <SDL/SDL_ttf.h>
+#include "bg.h"
+
+int main()
+{
+backg("background");
+
+return 0;
+}
---
+++ bg.h
@@ -0,0 +1,6 @@
+#ifndef FONCTIONS_H_
+#define FONCTIONS_H_
+void backg(char nom_fich[]);
+
+
+#endif /* FONCTIONS_H_ */
