3eabe209bab76bf840fb6434fc792fd76ffaf1fe
Emnaaouakassi <47668283+Emnaaouakassi@users.noreply.github.com>
2019-04-12 09:51:29 +0100

    Add files via upload

---
+++ Détection de collision entre deux entités en mouvement Bounding box/bounding.h
@@ -0,0 +1,12 @@
+#ifndef MENU_H_INCLUDED
+#define MENU_H_INCLUDED
+ 
+typedef struct rect_collision 
+{
+
+ int hauteur ; 
+int largeur ; 
+SDL_Rect position; 
+} rect_collision;
+
+#endif
---
+++ Détection de collision entre deux entités en mouvement Bounding box/main.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <SDL/SDL.h>
+#include <SDL/SDL_image.h>
+#include <SDL/SDL_mixer.h>
+#include "bounding.h"
+
+int main()
+{
+ 
+int Collision_Bounding_Box (SDL_Rect rec1 ,SDL_Rect position_obstacle);
+}
+
+
